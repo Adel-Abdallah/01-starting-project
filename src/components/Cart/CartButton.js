0779011375439341be535a71d01bb486bc6e936c
Adel-Abdallah <adelgafo@gmail.com>
2021-05-02 01:58:26 +0200

    redux toolkit 2

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,20 @@
+import { useDispatch } from "react-redux";
+import classes from "./CartButton.module.css";
+import { uiActions } from "./../../store/ui-slice";
+
+const CartButton = props => {
+  const dispatch = useDispatch();
+
+  const toggleHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button onClick={toggleHandler} className={classes.button}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
